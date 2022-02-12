3b8ed2d331de2887caef75113993ace293d60a30
Tania Banerjee <tania.mishra@gmail.com>
2022-02-12 15:18:30 -0500

    starting to add the post processing code on top of Qian's code.

---
+++ postprocessing.cpp
@@ -0,0 +1,8 @@
+#include "postprocessing.hpp"
+
+vector<double> compute_lagrange_parameters(const char* filepath, double* recon, int nphi, int nnodes, int vx, int vy, int local_nodes, int offset,  double* breg_recon, double &pd_error_b, double &pd_error_a, double &density_error_b, double &density_error_a, double &upara_error_b, double &upara_error_a, double &tperp_error_b, double &tperp_error_a, double &tpara_error_b, double &tpara_error_a) 
+{
+    vector <double> lagranges;
+    return lagranges;
+}
+
---
+++ postprocessing.hpp
@@ -0,0 +1,10 @@
+#ifndef POST_PROCESSING_HPP
+#define POST_PROCESSING_HPP
+
+#include <vector>
+
+using namespace std;
+
+vector<double> compute_lagrange_parameters(const char* filepath, double* recon, int nphi, int nnodes, int vx, int vy, int local_nodes, int offset, double* breg_recon, double &pd_error_b, double &pd_error_a, double &density_error_b, double &density_error_a, double &upara_error_b, double &upara_error_a, double &tperp_error_b, double &tperp_error_a, double &tpara_error_b, double &tpara_error_a);
+
+#endif //POST_PROCESSING_HPP
