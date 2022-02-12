3f187fc7d01102c780af0f0126b2ffd7597ba998
Tania Banerjee <tania.mishra@gmail.com>
2022-02-12 16:59:45 -0500

    Added qoi computation

--- postprocessing.cpp
+++ postprocessing.cpp
@@ -3,6 +3,8 @@
 #include "adios2.h"
 #include "postprocessing.hpp"
 
+double determinant(double a[4][4], double k);
+double** cofactor(double num[4][4], double f);
 void set_vol(vector <double> &grid_vol,
     double f0_nvp, double f0_nmu, int nnodes, vector <double> &vol)
 {
@@ -66,8 +68,7 @@ void set_vth2(vector <double> &f0_T_ev, double nnodes, double sml_e_charge, doub
 void read_f0_params(const char* datapath, double* i_f,
     int nnodes, int nphi, int nsize, vector <double> &vol,
     vector <double> &vth, vector <double> &vp, vector <double> &mu_qoi,
-    vector <double> &vth2, double &sml_e_charge, double &ptl_mass,
-    vector <double> &den_ref)
+    vector <double> &vth2, double &sml_e_charge, double &ptl_mass)
 {
     sml_e_charge = 1.6022e-19;
     ptl_mass = 3.344e-27;
@@ -75,7 +76,7 @@ void read_f0_params(const char* datapath, double* i_f,
     adios2::ADIOS ad;
     adios2::IO read_vol_io = ad.DeclareIO("xgc_vol");
     char vol_file[2048];
-    sprintf(vol_file, "%sxgc.f0.mesh.bp", datapath);
+    sprintf(vol_file, "%s/xgc.f0.mesh.bp", datapath);
     adios2::Engine reader_vol = read_vol_io.Open(vol_file, adios2::Mode::Read);
 
     adios2::Variable<double> var_i_f_in;
@@ -110,16 +111,105 @@ void read_f0_params(const char* datapath, double* i_f,
     std::vector<double> f0_T_ev;
     reader_vol.Get<double>(var_i_f_in, f0_T_ev);
 
-    var_i_f_in = read_vol_io.InquireVariable<double>("f0_den");
-    vector<std::size_t> den_shape = var_i_f_in.Shape();
-    var_i_f_in.SetSelection(adios2::Box<adios2::Dims>({0}, {den_shape[0]}));
-    reader_vol.Get<double>(var_i_f_in, den_ref);
-    reader_vol.Close();
-
     set_vol(grid_vol, f0_nvp[0], f0_nmu[0], nnodes, vol);
     set_vp(f0_nvp[0], f0_dvp[0], vp);
     set_mu_qoi(f0_nmu[0], f0_dsmu[0], mu_qoi);
     set_vth2(f0_T_ev, nnodes, sml_e_charge, ptl_mass, vth2, vth);
+}
+
+bool isConverged(vector <double> difflist, double eB)
+{
+    bool status = false;
+    unsigned int vsize = difflist.size();
+    if (vsize < 2) {
+         return status;
+    }
+    double last2Val = difflist[vsize-2];
+    double last1Val = difflist[vsize-1];
+    if (abs(last2Val - last1Val) < eB) {
+        status = true;
+    }
+    return status;
+}
+
+void compute_C_qois(double* i_f, int iphi, int nnodes, int nsize,
+    vector <double> &vol, vector <double> &vth,
+    vector <double> &vp, vector <double> &mu_qoi, vector <double> &vth2,
+    double ptl_mass, double sml_e_charge, vector <double> &den_f,
+    vector <double> &upara_f, vector <double> &tperp_f,
+    vector <double> &tpara_f)
+{
+    vector <double> den;
+    vector <double> upar;
+    vector <double> upar_;
+    vector <double> tper;
+    vector <double> en;
+    vector <double> T_par;
+    int i, j, k;
+    double* f0_f = &i_f[iphi*nnodes*nsize*nsize];
+    int den_index = iphi*nnodes;
+
+    for (i=0; i<nnodes*nsize*nsize; ++i) {
+        den.push_back(f0_f[i] / vol[i]);
+    }
+
+    double value = 0;
+    for (i=0; i<nnodes; ++i) {
+        value = 0;
+        for (j=0; j<nsize*nsize; ++j) {
+            value += den[nsize*nsize*i + j];
+        }
+        den_f.push_back(value);
+    }
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                upar.push_back(f0_f[nsize*nsize*i + nsize*j + k] *
+                    vol[nsize*nsize*i + nsize*j + k] *
+                    vth[i]*vp[k]);
+    }
+    for (i=0; i<nnodes; ++i) {
+        double value = 0;
+        for (j=0; j<nsize*nsize; ++j) {
+            value += upar[nsize*nsize*i + j];
+        }
+        upara_f.push_back(value/den_f[den_index + i]);
+    }
+    for (i=0; i<nnodes; ++i) {
+        upar_.push_back(upara_f[den_index + i]/vth[i]);
+    }
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                tper.push_back(f0_f[nsize*nsize*i + nsize*j + k] *
+                    vol[nsize*nsize*i + nsize*j + k] * 0.5 *
+                    mu_qoi[j] * vth2[i] * ptl_mass);
+    }
+    for (i=0; i<nnodes; ++i) {
+        double value = 0;
+        for (j=0; j<nsize*nsize; ++j) {
+            value += tper[nsize*nsize*i + j];
+        }
+        tperp_f.push_back(value/den_f[den_index + i]/sml_e_charge);
+    }
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            en.push_back(0.5*pow((vp[j]-upar_[i]),2));
+    }
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                T_par.push_back(f0_f[nsize*nsize*i + nsize*j + k] *
+                    vol[nsize*nsize*i + nsize*j + k] *
+                    en[nsize*i+k] * vth2[i] * ptl_mass);
+    }
+    for (i=0; i<nnodes; ++i) {
+        double value = 0;
+        for (j=0; j<nsize*nsize; ++j) {
+            value += T_par[nsize*nsize*i + j];
+        }
+        tpara_f.push_back(2.0*value/den_f[den_index + i]/sml_e_charge);
+    }
 }
 
 char* getDataPath(const char* filepath)
@@ -139,7 +229,55 @@ char* getDataPath(const char* filepath)
     return datapath;
 }
 
-vector<double> compute_lagrange_parameters(const char* filepath, double* recon, int local_elements, int local_nodes, double* i_f, int nphi, int nnodes, int vx, int vy, int offset,  double* breg_recon, double &pd_error_b, double &pd_error_a, double &density_error_b, double &density_error_a, double &upara_error_b, double &upara_error_a, double &tperp_error_b, double &tperp_error_a, double &tpara_error_b, double &tpara_error_a)
+vector <double> qoi_V2(vector <double> &vol, vector <double> &vth,
+    vector <double> &vp, int nnodes, int nsize)
+{
+    int i, j, k;
+    vector <double> V2;
+
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                V2.push_back(vol[nsize*nsize*i + nsize*j + k] *
+                    vth[i]*vp[k]);
+    }
+
+    return V2;
+}
+
+vector <double> qoi_V3(vector <double> &vol, vector <double> &vth2,
+    vector <double> &mu_qoi, double ptl_mass, int nnodes, int nsize)
+{
+    int i, j, k;
+    vector <double> V3;
+
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                V3.push_back(vol[nsize*nsize*i + nsize*j + k] * 0.5 *
+                    mu_qoi[j] * vth2[i] * ptl_mass);
+    }
+
+    return V3;
+}
+
+vector <double> qoi_V4(vector <double> &vol, vector <double> &vth2,
+    vector <double> &vp, double ptl_mass, int nnodes, int nsize)
+{
+    int i, j, k;
+    vector <double> V4;
+
+    for (i=0; i<nnodes; ++i) {
+        for (j=0; j<nsize; ++j)
+            for (k=0; k<nsize; ++k)
+                V4.push_back(vol[nsize*nsize*i + nsize*j + k] *
+                    vp[k]*vp[k] * vth2[i] * ptl_mass);
+    }
+
+    return V4;
+}
+
+vector<double> compute_lagrange_parameters(const char* filepath, double* recon, int local_elements, int local_nnodes, double* i_f, int nphi, int nnodes, int vx, int vy, int offset,  double* breg_recon, double &pd_error_b, double &pd_error_a, double &density_error_b, double &density_error_a, double &upara_error_b, double &upara_error_a, double &tperp_error_b, double &tperp_error_a, double &tpara_error_b, double &tpara_error_a)
 {
     // Remove non-negative values from input
     clock_t start = clock();
@@ -155,9 +293,235 @@ vector<double> compute_lagrange_parameters(const char* filepath, double* recon, 
     // Set datapath
     char* datapath = getDataPath(filepath);
 
-    // read_f0_params (datapath, i_f, nnodes, nphi, vx, vol, vth, vp,
-        // mu_qoi, vth2, sml_e_charge, ptl_mass);
+    // Revisit for precise params
+    read_f0_params (datapath, i_f, nnodes, nphi, vx, vol, vth, vp,
+        mu_qoi, vth2, sml_e_charge, ptl_mass);
+
+    vector <double> den_f, upara_f, tperp_f, tpara_f;
+    int iphi = 0;
+    for (iphi=0; iphi<nphi; ++iphi) {
+        compute_C_qois(i_f, iphi, local_nnodes, vx, vol, vth, vp, mu_qoi, vth2, ptl_mass, sml_e_charge, den_f, upara_f, tperp_f, tpara_f);
+    }
+
     vector <double> lagranges;
+#if 0
+    int count = 0;
+    vector <double> recon_breg;
+    double gradients[4] = {0.0, 0.0, 0.0, 0.0};
+    double hessians[4][4] = {0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0,
+                         0.0, 0.0, 0.0, 0.0};
+    double K[vx*vy];
+    double breg_result[vx*vy];
+    memset(K, 0, vx*vy*sizeof(double));
+    vector <double> V2 = qoi_V2(vol, vth, vp, nnodes, vx);
+    vector <double> V3 = qoi_V3(vol, vth2, mu_qoi, ptl_mass, nnodes, vx);
+    vector <double> V4 = qoi_V4(vol, vth2, vp, ptl_mass, nnodes, vx);
+    vector <double> tpara_data;
+    int p, idx, i;
+    for (p=0; p<8; ++p) {
+        for (i=0; i<nnodes; ++i) {
+            tpara_data.push_back(sml_e_charge * tpara_f[nnodes*p + i] +
+                vth2[i] * ptl_mass * pow((upara_f[nnodes*p + i]/vth[i]), 2));
+        }
+    }
+    // compare vectors V2, V3, V4, and Tpara
+    start = clock();
+    for (p=0; p<8; ++p) {
+        double* D = &den_f[p*nnodes];
+        double* U = &upara_f[p*nnodes];
+        double* Tperp = &tperp_f[p*nnodes];
+        double* Tpara = &tpara_data[p*nnodes];
+        int count_unLag = 0;
+        vector <int> node_unconv;
+        double maxD = -99999;
+        double maxU = -99999;
+        double maxTperp = -99999;
+        double maxTpara = -99999;
+        for (i=0; i<nnodes; ++i) {
+            if (D[i] > maxD) {
+                maxD = D[i];
+            }
+            if (U[i] > maxU) {
+                maxU = U[i];
+            }
+            if (Tperp[i] > maxTperp) {
+                maxTperp = Tperp[i];
+            }
+            if (Tpara[i] > maxTpara) {
+                maxTpara = Tpara[i];
+            }
+        }
+        double DeB = pow(maxD*1e-09, 2);
+        double UeB = pow(maxU*1e-09, 2);
+        double TperpEB = pow(maxTperp*1e-09, 2);
+        double TparaEB = pow(maxTpara*1e-09, 2);
+        for (idx=0; idx<nnodes; ++idx) {
+            double* recon_one = &recon[nnodes*vx*vy*p + vx*vy*idx];
+            double lambdas[4] = {0.0, 0.0, 0.0, 0.0};
+            vector <double> L2_den;
+            vector <double> L2_upara;
+            vector <double> L2_tperp;
+            vector <double> L2_tpara;
+            count = 0;
+            double aD = D[idx]*sml_e_charge;
+            while (1) {
+                for (i=0; i<vx*vy; ++i) {
+                    K[i] = lambdas[0]*vol[vx*vy*idx + i] +
+                           lambdas[1]*V2[vx*vy*idx + i] +
+                           lambdas[2]*V3[vx*vy*idx + i] +
+                           lambdas[3]*V4[vx*vy*idx + i];
+                }
+                double update_D=0, update_U=0, update_Tperp=0, update_Tpara=0;
+                if (count > 0) {
+                    for (i=0; i<vx*vy; ++i) {
+                        breg_result[i] = recon_one[i]*
+                            exp(-K[i]);
+                        update_D += breg_result[i]*vol[
+                            vx*vy*idx + i];
+                        update_U += breg_result[i]*V2[
+                            vx*vy*idx + i]/D[idx];
+                        update_Tperp += breg_result[i]*V3[
+                            vx*vy*idx + i]/aD;
+                        update_Tpara += breg_result[i]*V4[
+                            vx*vy*idx + i]/D[idx];
+                    }
+                    L2_den.push_back(pow((update_D - D[idx]), 2));
+                    L2_upara.push_back(pow((update_U - U[idx]), 2));
+                    L2_tperp.push_back(pow((update_Tperp-Tperp[idx]), 2));
+                    L2_tpara.push_back(pow((update_Tpara-Tpara[idx]), 2));
+                    // L2_den.push_back(update_D);
+                    // L2_upara.push_back(update_U);
+                    // L2_tperp.push_back(update_Tperp);
+                    // L2_tpara.push_back(update_Tpara);
+                    bool c1, c2, c3, c4;
+                    bool converged = (isConverged(L2_den, DeB)
+                        && isConverged(L2_upara, UeB)
+                        && isConverged(L2_tpara, TparaEB)
+                        && isConverged(L2_tperp, TperpEB));
+                    if (converged) {
+                        for (i=0; i<vx*vy; ++i) {
+                            recon_breg.push_back(breg_result[i]);
+                        }
+                        lagranges.push_back(lambdas[0]);
+                        lagranges.push_back(lambdas[1]);
+                        lagranges.push_back(lambdas[2]);
+                        lagranges.push_back(lambdas[3]);
+                        if (idx % 2000 == 0) {
+                            printf ("node %d finished\n", idx);
+                        }
+                        break;
+                    }
+                    else if (count == 20 && !converged) {
+                        for (i=0; i<vx*vy; ++i) {
+                            recon_breg.push_back(recon_one[i]);
+                        }
+                        lagranges.push_back(lambdas[0]);
+                        lagranges.push_back(lambdas[1]);
+                        lagranges.push_back(lambdas[2]);
+                        lagranges.push_back(lambdas[3]);
+                        printf ("Node %d did not converge\n", idx);
+                        count_unLag = count_unLag + 1;
+                        node_unconv.push_back(idx);
+                        break;
+                    }
+                }
+                double gvalue1 = D[idx], gvalue2 = U[idx]*D[idx];
+                double gvalue3 = Tperp[idx]*aD, gvalue4 = Tpara[idx]*D[idx];
+                double hvalue1 = 0, hvalue2 = 0, hvalue3 = 0, hvalue4 = 0;
+                double hvalue5 = 0, hvalue6 = 0, hvalue7 = 0;
+                double hvalue8 = 0, hvalue9 = 0, hvalue10 = 0;
+
+#if 0
+                for (i=0; i<2*vx; ++i) {
+                    gvalue3 += recon_one[i]*
+                          V3[vx*vy*idx + i]*exp(-K[i])*-1.0;
+                    printf ("i=%d, gvalue = %g, recon = %g, vol = %g, exp = %g, den = %g\n", i, gvalue3, recon_one[i], V3[vx*vy*idx + i], exp(-K[i]),  Tperp[idx]*aD);
+                }
+#endif
+                for (i=0; i<vx*vy; ++i) {
+                    gvalue1 += recon_one[i]*vol[vx*vy*idx + i]*
+                      exp(-K[i])*-1.0;
+                    gvalue2 += recon_one[i]*
+                      V2[vx*vy*idx + i]*exp(-K[i])*-1.0;
+                    gvalue3 += recon_one[i]*
+                      V3[vx*vy*idx + i]*exp(-K[i])*-1.0;
+                    gvalue4 += recon_one[i]*
+                      V4[vx*vy*idx + i]*exp(-K[i])*-1.0;
+
+                    hvalue1 += recon_one[i]*pow(
+                        vol[vx*vy*idx + i], 2)*exp(-K[i]);
+                    hvalue2 += recon_one[i]* vol[
+                        vx*vy*idx + i]*V2[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue3 += recon_one[i]* vol[
+                        vx*vy*idx + i]*V3[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue4 += recon_one[i]* vol[
+                        vx*vy*idx + i]*V4[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue5 += recon_one[i]*pow(
+                        V2[vx*vy*idx + i],2)*exp(-K[i]);
+                    hvalue6 += recon_one[i]*V2[
+                        vx*vy*idx + i]*V3[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue7 += recon_one[i]*V2[
+                        vx*vy*idx + i]*V4[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue8 += recon_one[i]*pow(
+                        V3[vx*vy*idx + i],2)*exp(-K[i]);
+                    hvalue9 += recon_one[i]*V3[
+                        vx*vy*idx + i]*V4[vx*vy*idx + i]*
+                        exp(-K[i]);
+                    hvalue10 += recon_one[i]*pow(
+                        V4[vx*vy*idx + i],2)*exp(-K[i]);
+                }
+                gradients[0] = gvalue1;
+                gradients[1] = gvalue2;
+                gradients[2] = gvalue3;
+                gradients[3] = gvalue4;
+                hessians[0][0] = hvalue1;
+                hessians[0][1] = hvalue2;
+                hessians[0][2] = hvalue3;
+                hessians[0][3] = hvalue4;
+                hessians[1][0] = hvalue2;
+                hessians[1][1] = hvalue5;
+                hessians[1][2] = hvalue6;
+                hessians[1][3] = hvalue7;
+                hessians[2][0] = hvalue3;
+                hessians[2][1] = hvalue6;
+                hessians[2][2] = hvalue8;
+                hessians[2][3] = hvalue9;
+                hessians[3][0] = hvalue4;
+                hessians[3][1] = hvalue7;
+                hessians[3][2] = hvalue9;
+                hessians[3][3] = hvalue10;
+                // compute lambdas
+                int order = 4;
+                int k;
+                double d = determinant(hessians, order);
+                if (d == 0) {
+                    printf ("Need to define pesudoinverse for matrix in node %d\n", idx);
+                }
+                else{
+                    double** inverse = cofactor(hessians, order);
+                    double matmul[4] = {0, 0, 0, 0};
+                    for (i=0; i<4; ++i) {
+                        matmul[i] = 0;
+                        for (k=0; k<4; ++k) {
+                            matmul[i] += inverse[i][k] * gradients[k];
+                        }
+                    }
+                    lambdas[0] = lambdas[0] - matmul[0];
+                    lambdas[1] = lambdas[1] - matmul[1];
+                    lambdas[2] = lambdas[2] - matmul[2];
+                    lambdas[3] = lambdas[3] - matmul[3];
+                }
+                count = count + 1;
+            }
+        }
+    }
+#endif
     return lagranges;
 }
-
