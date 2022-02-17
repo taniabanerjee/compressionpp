72250c5b9eda5fe4173fbbbb4f8fcd8fa2f27627
Tania Banerjee <tania.mishra@gmail.com>
2022-02-17 08:48:18 -0500

    added the inverse matrix file

---
+++ inverse_matrix.cpp
@@ -0,0 +1,109 @@
+#include<stdio.h>
+#include<math.h>
+double determinant(double [][4], double);
+double** cofactor(double [][4], double);
+double** transpose(double [][4], double [][4], double);
+ 
+/*For calculating Determinant of the Matrix */
+double determinant(double a[4][4], double k)
+{
+  double s = 1, det = 0;
+  double b[4][4] = {{0.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0}, {0.0, 0.0, 0.0, 0.0}};
+  int i, j, m, n, c;
+  if (k == 1)
+    {
+     return (a[0][0]);
+    }
+  else
+    {
+     det = 0;
+     for (c = 0; c < k; c++)
+       {
+        m = 0;
+        n = 0;
+        for (i = 0;i < k; i++)
+          {
+            for (j = 0 ;j < k; j++)
+              {
+                b[i][j] = 0;
+                if (i != 0 && j != c)
+                 {
+                   b[m][n] = a[i][j];
+                   if (n < (k - 2))
+                    n++;
+                   else
+                    {
+                     n = 0;
+                     m++;
+                     }
+                   }
+               }
+             }
+          det = det + s * (a[0][c] * determinant(b, k - 1));
+          s = -1 * s;
+          }
+    }
+ 
+    return (det);
+}
+ 
+double** cofactor(double num[4][4], double f)
+{
+ double b[4][4], fac[4][4];
+ int p, q, m, n, i, j;
+ for (q = 0;q < f; q++)
+ {
+   for (p = 0;p < f; p++)
+    {
+     m = 0;
+     n = 0;
+     for (i = 0;i < f; i++)
+     {
+       for (j = 0;j < f; j++)
+        {
+          if (i != q && j != p)
+          {
+            b[m][n] = num[i][j];
+            if (n < (f - 2))
+             n++;
+            else
+             {
+               n = 0;
+               m++;
+               }
+            }
+        }
+      }
+      fac[q][p] = pow(-1, q + p) * determinant(b, f - 1);
+    }
+  }
+  return transpose(num, fac, f);
+}
+/*Finding transpose of matrix*/ 
+double** transpose(double num[4][4], double fac[4][4], double r)
+{
+  int i, j;
+  double b[4][4], d;
+  double** inverse = new double* [4];
+  inverse[0] = new double[4];
+  inverse[1] = new double[4];
+  inverse[2] = new double[4];
+  inverse[3] = new double[4];
+ 
+  for (i = 0;i < r; i++)
+    {
+     for (j = 0;j < r; j++)
+       {
+         b[i][j] = fac[j][i];
+        }
+    }
+  d = determinant(num, r);
+  for (i = 0;i < r; i++)
+    {
+     for (j = 0;j < r; j++)
+       {
+        inverse[i][j] = b[i][j] / d;
+        }
+    }
+    return inverse;
+}
